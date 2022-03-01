10466a56917b319ace48752800c49640d72d163a
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-03-01 21:55:23 +0600

    card apply

--- my-phone.js
+++ my-phone.js
@@ -10,22 +10,21 @@ const searchPhone = () => {
     .then(res => res.json())
     .then(data => showSearchResult(data.data));
 }
-// const showSearchResult = data => {
-// const searchResult = document.getElementById('search-result');
-// data.forEach(data => {
-//     console.log(data);
-//     const div = document.createElement('div');
-//     div.classList.add('col');
-//     div.innerHTML = `
-//     <div class="card">
-//         <img src="${data.image}" class="card-img-top" alt="">
-//         <div class="card-body">
-//             <h3 class="card-brand">${data.brand}</h3>
-//             <h5 class="card-title">${data.phone_name}</h5>
-//             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-//         </div>
-//   </div>`
-//   searchResult.appendChild(div);
-// });
-
-// }
+const showSearchResult = data => {
+const searchResult = document.getElementById('search-result');
+data.forEach(data => {
+    console.log(data);
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+    <div class="card">
+        <img src="${data.image}" class="card-img-top" alt="">
+        <div class="card-body">
+            <h3 class="card-brand">${data.brand}</h3>
+            <h5 class="card-title">${data.phone_name}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+  </div>`
+  searchResult.appendChild(div);
+});
+}
