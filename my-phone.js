7995a92d800e5c95873d75e1abaa923be120075c
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-03-01 21:54:29 +0600

    fetch url

--- my-phone.js
+++ my-phone.js
@@ -2,14 +2,14 @@
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText);
-//     searchField.value = '';
-//     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-//     // console.log(url);
-//     fetch(url)
-//     .then(res => res.json())
-//     .then(data => showSearchResult(data.data));
-// }
+    // console.log(searchText);
+    searchField.value = '';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showSearchResult(data.data));
+}
 // const showSearchResult = data => {
 // const searchResult = document.getElementById('search-result');
 // data.forEach(data => {
